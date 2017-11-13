3cbe23d2e7a6f124e689371bdab2daf8b2e6fdbd
JordLoop <JordLoop@gmail.com>
2017-11-13 15:29:53 -0800

    import required dependencies for angular material library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenav } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { LandingComponent } from './landing/landing.component';
@@ -18,7 +21,9 @@ import { ExperienceComponent } from './experience/experience.component';
     ExperienceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenav
   ],
   providers: [],
   bootstrap: [AppComponent]
