be2702c8c310c3aed09a5970a1be3b8de4c5414f
JordLoop <JordLoop@gmail.com>
2017-11-13 14:38:11 -0800

    add new components to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { LandingComponent } from './landing/landing.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    LandingComponent
+    LandingComponent,
+    ContactComponent,
+    ProjectsComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule
