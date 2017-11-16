9b3b4fc0ad9a96aecadfcd02cefbed51890948d0
JordLoop <JordLoop@gmail.com>
2017-11-16 13:53:50 -0800

    add experiences.model and use ngFor to display each experience

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Experience } from '../models/experience.model';
 
 @Component({
   selector: 'experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+  styleUrls: ['./experience.component.css'],
 })
 export class ExperienceComponent implements OnInit {
 
@@ -11,5 +12,11 @@ export class ExperienceComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  experiences: Experience[] = [
+    new Experience('Flight', 'Intern', "ScatterSchool is an Angular2 web application that took advantage of Firebase's real time database to provide a place for makers to colaborate, write tutorials, and build comunity. I primarially worked with the services that provided the components with the required data."),
+    new Experience('Flight', 'Intern', "ScatterSchool is an Angular2 web application that took advantage of Firebase's real time database to provide a place for makers to colaborate, write tutorials, and build comunity. I primarially worked with the services that provided the components with the required data."),
+    new Experience('Flight', 'Intern', "ScatterSchool is an Angular2 web application that took advantage of Firebase's real time database to provide a place for makers to colaborate, write tutorials, and build comunity. I primarially worked with the services that provided the components with the required data.")
+  ]
 
 }
---
+++ src/app/models/experience.model.ts
@@ -0,0 +1,4 @@
+export class Experience {
+    constructor(public workplace: string, public position: string, public body: string) {  }
+
+}
