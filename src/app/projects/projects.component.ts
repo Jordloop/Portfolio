6041f379cdbc9a999f2ce932e705ca938e4f5d69
JordLoop <Jordloop@gmail.com>
2017-11-28 15:00:29 -0800

    remove app-brasking "

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -16,7 +16,7 @@ export class ProjectsComponent implements OnInit {
   projects: Project[] = [
     new Project('Campsite Finder', "https://u.cubeupload.com/Bompkins/projectcampsite.png", "https://github.com/Jordloop/Campsite", null),
     new Project('Cutthroat Caverns', "https://u.cubeupload.com/Bompkins/projectCC.png", "https://github.com/Jordloop/Cutthroat-Cavern-Web-App", null),
-    new Project('Son of Cod', "https://u.cubeupload.com/Bompkins/projectSOC.png"", "https://github.com/Jordloop/SonOfCod", null),
+    new Project('Son of Cod', "https://u.cubeupload.com/Bompkins/projectSOC.png", "https://github.com/Jordloop/SonOfCod", null),
   ]
 
 }
