5b9b2b066c694798b28f1aa2b6d2fac2ed672319
JordLoop <Jordloop@gmail.com>
2017-11-28 14:55:20 -0800

    remove unused properties from project.model

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -1,7 +1,5 @@
 export class Project {
     constructor(public title: string,
-                public subtitle: string,
-                public body: string,
                 public screenshot: string,
                 public repo: string,
                 public host: string
